fe7075a19d2bd58d33268625c2e6af45bf0eb0c8
TechLakhan <patillakhan25@gmail.comgit config --global user.name TechLakhangit config --global user.email patillakhan25@gmail.com>
2026-08-03 22:50:26 +0530

    spring abstraction

--- src/main/java/co/in/techLakhan/Employee_Cache_demo/config/RedisConfig.java
+++ src/main/java/co/in/techLakhan/Employee_Cache_demo/config/RedisConfig.java
@@ -1,12 +1,17 @@
 package co.in.techLakhan.Employee_Cache_demo.config;
 
+import org.springframework.cache.CacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.time.Duration;
 
 @Configuration
 public class RedisConfig {
@@ -20,4 +25,17 @@ public class RedisConfig {
         return redisTemplate;
     }
 
+    @Bean
+    public CacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
+        RedisCacheConfiguration defaultConfig = RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofMinutes(5))
+                .serializeValuesWith(RedisSerializationContext.SerializationPair
+                        .fromSerializer(new GenericJackson2JsonRedisSerializer())
+                )
+                .disableCachingNullValues();
+
+        return RedisCacheManager.builder(redisConnectionFactory)
+                .cacheDefaults(defaultConfig)
+                .build();
+    }
 }
--- src/main/java/co/in/techLakhan/Employee_Cache_demo/service/EmployeeService.java
+++ src/main/java/co/in/techLakhan/Employee_Cache_demo/service/EmployeeService.java
@@ -4,20 +4,13 @@ import co.in.techLakhan.Employee_Cache_demo.entity.Employee;
 import co.in.techLakhan.Employee_Cache_demo.exception.EmployeeNotFoundException;
 import co.in.techLakhan.Employee_Cache_demo.repository.EmployeeRepository;
 import co.in.techLakhan.Employee_Cache_demo.util.EmployeeCacheProperties;
-import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.TimeoutUtils;
-import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-
-import java.io.Serializable;
-import java.util.Optional;
-import java.util.concurrent.TimeUnit;
 
 import static co.in.techLakhan.Employee_Cache_demo.constants.ErrorCodes.EXP001;
 
@@ -38,33 +31,22 @@ public class EmployeeService {
         this.redisTemplate = redisTemplate;
         this.cacheProperties = cacheProperties;
     }
+    @Cacheable(value = "employees", key = "#id")
     public Employee getEmployeeById(Long id) throws EmployeeNotFoundException {
-        String key = getEmployeeCacheKey(id);
-        Employee cachedEmployee = (Employee) redisTemplate.opsForValue().get(key);
-        if (cachedEmployee != null) {
-            logger.info("Cache hit for key {}", key);
-            return cachedEmployee;
-        } else {
-            logger.info("Cache miss for key {}", key);
-            Employee employee = employeeRepository.findById(id).orElseThrow(() -> new EmployeeNotFoundException(404, EXP001.message(), HttpStatus.NOT_FOUND));
-            redisTemplate.opsForValue().set(key, employee, cacheProperties.getTtl(), cacheProperties.getUnit());
-            return employee;
-        }
+        logger.info("Fetching employee {} from database", id);
+        Employee employee = employeeRepository.findById(id).orElseThrow(() -> new EmployeeNotFoundException(404, EXP001.message(), HttpStatus.NOT_FOUND));
+        return employee;
     }
 
-    private String getEmployeeCacheKey(Long id){
-        return "Employee:" + id;
-    }
-
+    @CacheEvict(value = "employees", key = "#newDetails.id")
     public void updateEmployeeDetails(Employee newDetails) throws EmployeeNotFoundException {
         Long employeeId = newDetails.getId();
         Employee employee = employeeRepository.findById(employeeId)
-                .orElseThrow(() -> new EmployeeNotFoundException(404, EXP001.message(), HttpStatus.NOT_FOUND)));
+                .orElseThrow(() -> new EmployeeNotFoundException(404, EXP001.message(), HttpStatus.NOT_FOUND));
         employee.setId(employeeId);
         employee.setName(newDetails.getName());
         employee.setDepartment(newDetails.getDepartment());
         employee.setSalary(newDetails.getSalary());
         employeeRepository.save(employee);
-        redisTemplate.delete(getEmployeeCacheKey(employeeId));
     }
 }
